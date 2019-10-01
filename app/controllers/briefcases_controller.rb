cc66faf9330f6d3ed591c02c200d3c9ad034a0c5
Olivier <orohellec44@gmail.com>
2019-10-01 19:50:47 +0200

    Added new and edit buttons for briefcase items

--- app/controllers/briefcases_controller.rb
+++ app/controllers/briefcases_controller.rb
@@ -1,4 +1,6 @@
 class BriefcasesController < ApplicationController
+  before_action :set_briefcase, only: [:show, :edit, :update, :destroy]
+
   def index
     @briefcase_items = Briefcase.all
   end
@@ -12,17 +14,30 @@ class BriefcasesController < ApplicationController
 
     respond_to do |format|
       if @briefcase_item.save
-        format.html { redirect_to briefcases_path, notice: 'Blog was successfully created.' }
+        format.html { redirect_to briefcases_path, notice: 'Portfolio was successfully created.' }
       else
         format.html { render :new }
       end
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @briefcase_item.update(briefcase_params)
+        format.html { redirect_to briefcases_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_blog
-    @briefcase = Briefcase.find(params[:id])
+  def set_briefcase
+    @briefcase_item = Briefcase.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
