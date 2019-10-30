112aa1297f95e72488982b80c7585068bd557265
Olivier <orohellec44@gmail.com>
2019-10-30 21:20:08 +0100

    Implemented concern for managing image data

--- app/models/briefcase.rb
+++ app/models/briefcase.rb
@@ -1,4 +1,5 @@
 class Briefcase < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -11,7 +12,7 @@ class Briefcase < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://place-hold.it/600x400"
-    self.thumb_image ||= "https://place-hold.it/300x200"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '300', width: '200')
   end
 end
