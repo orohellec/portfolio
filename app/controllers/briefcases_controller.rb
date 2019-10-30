a508962be126bc7a51886b94c9fbe536471b34c0
Olivier <orohellec44@gmail.com>
2019-10-30 14:21:29 +0100

    customize portfolios routes

--- app/controllers/briefcases_controller.rb
+++ app/controllers/briefcases_controller.rb
@@ -1,23 +1,23 @@
 class BriefcasesController < ApplicationController
-  before_action :set_briefcase, only: [:show, :edit, :update, :destroy]
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 
   def show
   end
 
   def index
-    @briefcase_items = Briefcase.all
+    @portfolios = Briefcase.all
   end
 
   def new
-    @briefcase_item = Briefcase.new
+    @portfolio = Briefcase.new
   end
 
   def create
-    @briefcase_item = Briefcase.new(briefcase_params)
+    @portfolio = Briefcase.new(portfolio_params)
 
     respond_to do |format|
-      if @briefcase_item.save
-        format.html { redirect_to briefcases_path, notice: 'Portfolio was successfully created.' }
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
       else
         format.html { render :new }
       end
@@ -29,8 +29,8 @@ class BriefcasesController < ApplicationController
 
   def update
     respond_to do |format|
-      if @briefcase_item.update(briefcase_params)
-        format.html { redirect_to briefcases_path, notice: 'Portfolio was successfully updated.' }
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -38,20 +38,20 @@ class BriefcasesController < ApplicationController
   end
 
   def destroy
-    @briefcase_item.destroy
+    @portfolio.destroy
     respond_to do |format|
-      format.html { redirect_to briefcases_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to portfolios_url, notice: 'Blog was successfully destroyed.' }
     end
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_briefcase
-    @briefcase_item = Briefcase.find(params[:id])
+  def set_portfolio
+    @portfolio = Briefcase.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  def briefcase_params
-    params.require(:briefcase).permit(:title, :subtitle, :body)
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :briefcases
+  # resources :briefcases, except: [:show]
+  resources :portfolios, controller: 'briefcases', except: [:show]
+  get 'portfolio/:id', to: 'briefcases#show', as: 'portfolio_show'
+
   root to: 'pages#home'
+
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
