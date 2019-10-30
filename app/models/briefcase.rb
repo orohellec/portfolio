4a9eac303466911051855286aab3c30fd1a1d62a
Olivier <orohellec44@gmail.com>
2019-10-30 21:04:08 +0100

    Implemented image default for portfolios items

--- app/models/briefcase.rb
+++ app/models/briefcase.rb
@@ -7,4 +7,11 @@ class Briefcase < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
   scope :angular_portfolio_items, -> { where(subtitle: 'Angular') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://place-hold.it/600x400"
+    self.thumb_image ||= "https://place-hold.it/300x200"
+  end
 end
